8800ee0bb618f3be04a166ec83f2e355be48cf72
Ethel Agubama <akanzirethyl@gmail.com>
2026-08-07 12:07:39 +0000

    Add list_events Lambda function with IAM role and DynamoDB read access

---
+++ terraform/iam.tf
@@ -0,0 +1,46 @@
+resource "aws_iam_role" "lambda_exec" {
+  name = "event-ticketing-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy" "lambda_dynamodb" {
+  name = "lambda-dynamodb-access"
+  role = aws_iam_role.lambda_exec.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+          "dynamodb:Query",
+          "dynamodb:Scan",
+          "dynamodb:UpdateItem"
+        ]
+        Resource = [
+          aws_dynamodb_table.events.arn,
+          aws_dynamodb_table.registrations.arn
+        ]
+      }
+    ]
+  })
+}
---
+++ terraform/lambda.tf
@@ -0,0 +1,15 @@
+data "archive_file" "list_events_zip" {
+  type        = "zip"
+  source_file = "${path.module}/../lambda/list_events/lambda_function.py"
+  output_path = "${path.module}/../lambda/list_events/list_events.zip"
+}
+
+resource "aws_lambda_function" "list_events" {
+  function_name = "list_events"
+  filename      = data.archive_file.list_events_zip.output_path
+  source_code_hash = data.archive_file.list_events_zip.output_base64sha256
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.12"
+  role          = aws_iam_role.lambda_exec.arn
+  timeout       = 10
+}
