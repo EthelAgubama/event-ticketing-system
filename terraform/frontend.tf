46199eec5f34dc71e6b149e97d742f68dda0761d
Ethel Agubama <akanzirethyl@gmail.com>
2026-08-07 21:22:49 +0000

    Add teal celebratory background with balloon decorations to frontend

---
+++ terraform/frontend.tf
@@ -0,0 +1,50 @@
+resource "aws_s3_bucket" "frontend" {
+  bucket = "event-ticketing-frontend-${data.aws_caller_identity.current.account_id}"
+}
+
+resource "aws_s3_bucket_public_access_block" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_website_configuration" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_policy" "frontend_public_read" {
+  bucket = aws_s3_bucket.frontend.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.frontend.arn}/*"
+      }
+    ]
+  })
+
+  depends_on = [aws_s3_bucket_public_access_block.frontend]
+}
+
+resource "aws_s3_object" "index_html" {
+  bucket       = aws_s3_bucket.frontend.id
+  key          = "index.html"
+  source       = "${path.module}/../frontend/index.html"
+  etag         = filemd5("${path.module}/../frontend/index.html")
+  content_type = "text/html"
+}
+
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,6 @@
 output "api_url" {
   value = "${aws_api_gateway_stage.dev.invoke_url}/events"
 }
+output "frontend_url" {
+  value = "http://${aws_s3_bucket.frontend.bucket}.s3-website-${data.aws_region.current.name}.amazonaws.com"
+}
